72b7afce46a660b4a2c2d6a1fae5052ccc814fd7
Kshitiz-b <95609931+Kshitiz-b@users.noreply.github.com>
2022-06-05 13:05:49 +0530

    commiting from web

---
+++ Cpp/52.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My first program on github vscode web!";
+    cout << endl << "Feeling great :sunglasses " << endl;
+
+    return 0;
+}
