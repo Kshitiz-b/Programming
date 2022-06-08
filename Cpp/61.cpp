5af700d58b5312d987684fc561a9e84b82de831f
Kshitiz-b <kshitizbhargava2626@gmail.com>
2022-06-08 18:07:48 +0530

    using G3

--- Cpp/61.cpp
+++ Cpp/61.cpp
@@ -1,3 +1,11 @@
+/* 
+STL(Standard Template Library)
+library of generic classes and functions
+components of STL
+1. Containers
+2. Algorithms
+3. Iterators
+*/ 
 #include <iostream>
 using namespace std;
 
