8341a3154b798203ad31787250d7b4d8f81d2892
Kshitiz-b <kshitizbhargava2626@gmail.com>
2022-07-20 19:44:00 +0530

    using G3

---
+++ Java/J1.java
@@ -0,0 +1,12 @@
+public class J1 {
+    public static void main(String []args)
+    {
+        int age = 37;
+        String name = "Kshitiz";
+        float weight = 70.5f;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(weight);
+    }
+}
---
+++ Java/J2.java
@@ -0,0 +1,11 @@
+public class J2 {
+    public static void main(String []args){
+        boolean isValid = true;
+        byte marks = 90;
+        float pi = 3.14f;
+        float div = 15.0f/4.0f;
+        long views = 10000000000000l;
+        char gender = 'M';
+
+    }
+}
