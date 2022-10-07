bbbf9c0adf83486bebd050f72c9ad8cd457b87c3
Kshitiz-b <kshitizbhargava2626@gmail.com>
2022-10-07 20:38:35 +0530

    using VS Code

--- Java/Question.java
+++ Java/Question.java
@@ -1,11 +1,8 @@
-public class Question
-{
-    public static void main (String[] args)
-{
-    int i =10;
-    int n = i++%2;
-    int k = ++i%7;
-    System.out.println(i+n+k);
+public class Question {
+    public static void main(String[] args) {
+        int i = 10;
+        int n = i++ % 2;
+        int k = ++i % 7;
+        System.out.println(i + n + k);
+    }
 }
-}
-
