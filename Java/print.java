35e02c4cdb70698d13ac2273d6a625d9525bcc04
Kshitiz-b <kshitizbhargava2626@gmail.com>
2022-10-27 15:49:59 +0530

    using VS Code

--- Java/print.java
+++ Java/print.java
@@ -3,9 +3,10 @@ public class print {
     public static void main(String []args)
     {
         System.out.println("What is your name?");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine(); 
-        System.out.println("Your name is :"+ name);
+        try (Scanner sc = new Scanner(System.in)) {
+            String name = sc.nextLine(); 
+            System.out.println("Your name is :"+ name);
+        }
 
     }
 }
