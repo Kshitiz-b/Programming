e74697cbdb46061048ea132279049f4efe04db70
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-01-07 21:41:46 +0530

    using VS Code

---
+++ Ruby/variable.rb
@@ -0,0 +1,9 @@
+phrase = "Giraffe Academy"
+character_name = "John"
+character_age = 35
+ismale = true
+puts ("Name: " + character_name.upcase())
+puts "Age: #{character_age}"
+puts "Male? #{ismale}"
+puts phrase[0, 7]
+puts phrase.index("ffe")
