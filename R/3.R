88ee9e23713905ecb67593c81e01903fb3545e95
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-01-15 17:22:39 +0530

    using VS Code

--- R/3.R
+++ R/3.R
@@ -1,13 +1,12 @@
-A <- matrix(nrow = 2, ncol = 2, data = c(1, 1, 2, 1))
-B <- matrix(nrow = 2, ncol = 2, data = c(0, 1, -1, 2))
+A <- matrix(nrow = 2, ncol = 2, data = c(1, 1, 2, 1)) # nolint
+B <- matrix(nrow = 2, ncol = 2, data = c(0, 1, -1, 2)) # nolint
 
-C <- A %*% B
-D <- solve(C)
+C <- A %*% B # nolint
+D <- solve(C) # nolint
 print(D)
 
-AT <- solve(A)
-BT <- solve(B)
+AT <- solve(A) # nolint
+BT <- solve(B) # nolint
 
-DT <- BT %*% AT
+DT <- BT %*% AT # nolint
 print(DT)
-
