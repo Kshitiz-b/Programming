ddfeee71a91d5f863819ecf6757f49461f37c7f8
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-01-20 20:17:28 +0530

    using VS Code

--- DAA/Divide and Conquer/Karatsuba.c
+++ DAA/Divide and Conquer/Karatsuba.c
@@ -11,25 +11,25 @@ int max(int x, int y)
 
 int karatsuba(int x, int y)
 {
-    if((x < 10) || (y<10))
-        return x*y;
+    if ((x < 10) || (y < 10))
+        return x * y;
     else
     {
-        int x_length = (int) log10(x) + 1;
-        int y_length = (int) log10(y) + 1;
+        int x_length = (int)log10(x) + 1;
+        int y_length = (int)log10(y) + 1;
         int n = max(x_length, y_length);
-        int half = n/2;
-        int a = x / pow(10, half);
-        int b = x % (pow(10, half)); // error
-        int c = y / pow(10, half);
-        int d = y % (pow(10, half)); // error
+        int half = n / 2;
+        int rem = pow(10, half);
+        int a = x / rem;
+        int b = x % (rem);
+        int c = y / rem;
+        int d = y % (rem);
 
         int ac = karatsuba(a, c);
         int bd = karatsuba(b, d);
-        int ad_plus_bc = karatsuba(a+b, c+d) - ac - bd;
-        int result = ac * pow(10, (2 * half)) + (ad_plus_bc * pow(10, half)) + bd;
+        int ad_plus_bc = karatsuba(a + b, c + d) - ac - bd;
+        int result = ac * pow(10, (2 * half)) + (ad_plus_bc * rem) + bd;
         return result;
-
     }
 }
 
