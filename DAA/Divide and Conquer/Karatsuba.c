0b72772abe003e0b83363cb8cea18e626d259576
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-01-20 20:35:08 +0530

    using VS Code

--- DAA/Divide and Conquer/Karatsuba.c
+++ DAA/Divide and Conquer/Karatsuba.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int max(int x, int y)
+int getSize(long num)
+{
+    int size = 0;
+    while (num > 0)
+    {
+        size++;
+        num /= 10;
+    }
+    return size;
+}
+
+long max(long x, long y)
 {
     if (x >= y)
         return x;
@@ -9,36 +20,45 @@ int max(int x, int y)
         return y;
 }
 
-int karatsuba(int x, int y)
+long karatsuba(long x, long y)
 {
     if ((x < 10) || (y < 10))
         return x * y;
     else
     {
-        int x_length = (int)log10(x) + 1;
-        int y_length = (int)log10(y) + 1;
-        int n = max(x_length, y_length);
+        // long x_length = (long)log10(x) + 1;
+        // long y_length = (long)log10(y) + 1;
+        int n = max(getSize(x), getSize(y));
         int half = n / 2;
-        int rem = pow(10, half);
-        int a = x / rem;
-        int b = x % (rem);
-        int c = y / rem;
-        int d = y % (rem);
+        // int rem = pow(10, half);
+        long rem = (long)ceil(pow(10, half));
 
-        int ac = karatsuba(a, c);
-        int bd = karatsuba(b, d);
-        int ad_plus_bc = karatsuba(a + b, c + d) - ac - bd;
-        int result = ac * pow(10, (2 * half)) + (ad_plus_bc * rem) + bd;
+        // long a = x / rem;
+        // long b = x % (rem);
+        // long c = y / rem;
+        // long d = y % (rem);
+
+        long a = (long)floor(x / (double)rem);
+        long b = x % rem;
+        long c = (long)floor(y / (double)rem);
+        long d = y % rem;
+
+        long ac = karatsuba(a, c);
+        long bd = karatsuba(b, d);
+        long ad_plus_bc = karatsuba(a + b, c + d) - ac - bd;
+        long result = (long)(ac * pow(10, (2 * half)) + (ad_plus_bc * rem) + bd);
+        //long result = (long)(a * pow(10 * 1l, 2 * n) + b * pow(10 * 1l, n) + c);
+
         return result;
     }
 }
 
 int main()
 {
-    int x, y;
+    long x, y;
     scanf("%d %d", &x, &y);
 
-    int ans = karatsuba(x, y);
+    long ans = karatsuba(x, y);
     printf("%d", ans);
     return 0;
 }
