19075859dc61e906bf13f85b940459026b5a2572
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-02-17 12:23:16 +0530

    using VS Code

--- R/6.R
+++ R/6.R
@@ -3,33 +3,28 @@ mid
 f <- c(5, 15, 28, 24, 17, 10, 1)
 fr.distr <- data.frame(mid, f)
 fr.distr
-
 mean <- (sum(mid * f)) / sum(f)
 mean
-
 midx <- seq(132, 162, 5)
 frequency <- c(5, 15, 28, 24, 17, 10, 1)
 fr.dist <- data.frame(midx, frequency)
 fr.dist
-
 cl <- cumsum(frequency)
 cl
 n <- sum(frequency)
 n
-ml <- min(which(cl >= n / 2))
+ml <- min(which(cl >= n / 2)) # The serial number of the median class
 ml
 h <- ml
 h
-f <- frequency[ml]
+f <- frequency[ml] # frequency of the median class
 f
-c <- cl[ml - 1]
+c <- cl[ml - 1] # cumulative frequency of the median class
 c
 l <- mid[ml] - h / 2
 l
-
-median <- l + (((n / 2) - c) / f) * h
+median <- l + (((n / 2) - c) / f) * h # median
 median
-
 m <- which(frequency == max(frequency)) # serial number of the median class
 m
 fm <- frequency[m] # frequency of the modal class
