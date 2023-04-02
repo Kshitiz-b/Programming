02025b29f959724a20b537aaedde92c301e24517
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-04-02 15:00:43 +0530

    using VS Code

--- R/task5_1.R
+++ R/task5_1.R
@@ -1,25 +1,15 @@
-#21BCE2067
-#Kshitiz Bhargava
+# 21BCE2067
+# Kshitiz Bhargava
 x <- 0:6
 f <- c(5, 18, 28, 12, 7, 6, 4)
-
-n <- sum(x * f)
-p <- sum(x * f) / sum(f)
-
-y <- dbinom(x, size = n, prob = p)
-
-chisq <- sum((f - n * y)^2 / (n * y))
-
-df <- length(x) - 1 # degrees of freedom
-pval <- 1 - pchisq(chisq, df)
-
-plot(x, f,
-    type = "h", lwd = 10, col = "blue",
-    xlab = "x", ylab = "Frequency", ylim = c(0, 30)
-)
-points(x, n * y, type = "h", lwd = 5, col = "red")
-legend("topright",
-    legend = c("Observed", "Expected"),
-    col = c("blue", "red"), lwd = 10, inset = 0.02
-)
-
+n <- 6
+mean <- sum(f * x) / sum(f)
+p <- mean / n
+p
+expf <- dbinom(x, n, p)
+f1 <- round(sum(f) * expf)
+sum(f)
+obf <- c(sum(expf[0:2]), expf[3:4], sum(expf[5:7]))
+exf <- c(sum(f[0:2]), f[3:4], sum(f[5:7]))
+chisq <- sum(((obf - exf)^2) / exf) / 12.19
+chisq
--- R/task5_2.R
+++ R/task5_2.R
@@ -1,14 +1,14 @@
-#21BCE2067
-#Kshitiz Bhargava
+# 21BCE2067
+# Kshitiz Bhargava
 x <- 0:6
-f <- c(275, 72, 30, 7, 5, 2, 1)
-lambda <- (sum(f * x) / sum(f1))
+f <- c(275, 72, 80, 7, 5, 2, 1)
+lambda <- (sum(f * x) / sum(f))
 expf <- dpois(x, lambda) * sum(f)
 f1 <- round(expf)
 sum(f)
 sum(f1)
 obf <- c(275, 72, 30, 15)
 exf <- c(242, 117, 28, 6)
-chisq <- sum(((sbf - exf)^2 / exf))
+chisq <- sum(((obf - exf)^2) / exf)
 chisq
 qchisq(0.95, 2)
