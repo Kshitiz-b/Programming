358db57cd3224c59fcc887e300ffd4ff5e06d27d
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-05-27 19:07:38 +0530

    using VS Code

--- ACC/toggleSwitch.java
+++ ACC/toggleSwitch.java
@@ -4,6 +4,7 @@ public class toggleSwitch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        System.out.println((int)Math.sqrt(n));
         Boolean[] b = new Boolean[n + 1];
         Arrays.fill(b, Boolean.TRUE);
 
