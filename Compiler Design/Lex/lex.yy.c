e34dfd3a951fb06d93f6397f4b1469eba22a5d44
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-07-20 12:16:51 +0530

    using VS Code

--- Compiler Design/Lex/lex.yy.c
+++ Compiler Design/Lex/lex.yy.c
@@ -282,29 +282,29 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
-static yyconst short int yy_accept[11] =
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
+static yyconst short int yy_accept[10] =
     {   0,
-        0,    0,    3,    2,    2,    0,    0,    0,    1,    0
+        0,    0,    4,    2,    3,    2,    0,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    3,    3,    4,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    5,    3,    3,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    3,    3,    4,    3,
 
-        2,    1,    1,    3,    1,    1,    1,    4,    1,    1,
-        5,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    5,    3,    3,    3,    3,
+        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -323,31 +323,31 @@ static yyconst int yy_ec[256] =
 
 static yyconst int yy_meta[6] =
     {   0,
-        1,    2,    1,    3,    4
+        1,    1,    2,    2,    2
     } ;
 
-static yyconst short int yy_base[16] =
+static yyconst short int yy_base[12] =
     {   0,
-        8,    7,    9,   12,    0,    0,    0,    0,   12,   12,
-        0,    6,    4,    3,    1
+        0,    3,   11,   12,   12,    5,    1,    0,   12,    7,
+        1
     } ;
 
-static yyconst short int yy_def[16] =
+static yyconst short int yy_def[12] =
     {   0,
-       11,   11,   10,   10,   12,   13,   14,   15,   10,    0,
-       10,   10,   10,   10,   10
+       10,   10,    9,    9,    9,   11,   11,    7,    0,    9,
+        9
     } ;
 
 static yyconst short int yy_nxt[18] =
     {   0,
-        4,    4,    4,    4,    9,    8,    7,    6,   10,    5,
-        5,    3,   10,   10,   10,   10,   10
+        9,    5,    7,    6,    5,    8,    6,    4,    4,    8,
+        9,    3,    9,    9,    9,    9,    9
     } ;
 
 static yyconst short int yy_chk[18] =
     {   0,
-       11,   11,   11,   11,   15,   14,   13,   12,    3,    2,
-        1,   10,   10,   10,   10,   10,   10
+        0,    1,   11,    1,    2,    7,    2,   10,   10,    6,
+        3,    9,    9,    9,    9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -365,9 +365,8 @@ char *yytext;
 #define INITIAL 0
 #line 2 "lextest.l"
     #include<stdio.h>
-    int vowel_count=0;
-    int con_count;
-#line 371 "lex.yy.c"
+
+#line 370 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -518,9 +517,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 "lextest.l"
+#line 5 "lextest.l"
 
-#line 524 "lex.yy.c"
+#line 523 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -571,7 +570,7 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -605,15 +604,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "lextest.l"
-{vowel_count=2;con_count=3;}
+#line 6 "lextest.l"
+{printf("%s", yytext);}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 9 "lextest.l"
+#line 7 "lextest.l"
+;
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 8 "lextest.l"
 ECHO;
 	YY_BREAK
-#line 617 "lex.yy.c"
+#line 621 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -905,7 +909,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -940,11 +944,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 9);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1499,7 +1503,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 9 "lextest.l"
+#line 8 "lextest.l"
 
 int yywrap(){
     return 1;
@@ -1507,7 +1511,5 @@ int yywrap(){
 int main(){
     printf("enter the string: ");
     yylex();
-    printf("Vowel Count: %d\n", vowel_count);
-    printf("Consonant Count: %d", con_count);
     return 0;
 }
