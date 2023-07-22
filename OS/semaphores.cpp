41bcb26bb12c3b89ef43e2f73c5968144d1bc7f2
Kshitiz-b <kshitizbhargava2626@gmail.com>
2023-07-22 10:26:41 +0530

    using VS Code

--- OS/semaphores.cpp
+++ OS/semaphores.cpp
@@ -1,55 +1,28 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-
 using namespace std;
-
-class Semaphore {
-public:
-    Semaphore(int count = 0) : count_(count) {}
-
-    void notify() {
-        unique_lock<mutex> lock(mutex_);
-        ++count_;
-        condition_.notify_one();
+int main()
+{
+    int n;
+    int critical_section = -1; // -1 means no proccess in critical section
+    cout << "Enter Proccesses Count: ";
+    cin >> n;
+    int binary_semaphore[n];
+    for (int i = 0; i < n; i++)
+    {
+        binary_semaphore[i] = 0; // Intial Semaphore = 0
     }
-
-    void wait() {
-        unique_lock<mutex> lock(mutex_);
-        condition_.wait(lock, [this]() { return count_ > 0; });
-        --count_;
+    for (int i = 0; i < n; i++)
+    {
+        binary_semaphore[i] = 1; // 1 means entered the critical section
+        cout << "\nProccess " << i + 1 << " enters the Critical Section" << endl;
+        cout << "Remaining Proccess\t  Semaphore\n";
+        cout << "--------------------------------------------\n";
+        for (int j = i; j < n; j++)
+        {
+            cout << "Process " << j + 1 << "\t\t\t" << binary_semaphore[j] << endl;
+        }
     }
-
-private:
-    mutex mutex_;
-    condition_variable condition_;
-    int count_;
-};
-
-Semaphore semaphore(1);  // Initialize the semaphore with a count of 1
-
-void worker(int id) {
-    cout << "Worker " << id << " is waiting." << endl;
-    semaphore.wait();  // Wait for the semaphore
-
-    cout << "Worker " << id << " starts working." << endl;
-    this_thread::sleep_for(chrono::seconds(2));  // Simulate some work
-
-    cout << "Worker " << id << " finishes working." << endl;
-    semaphore.notify();  // Release the semaphore
-}
-
-int main() {
-    thread workers[3];
-
-    for (int i = 0; i < 3; ++i) {
-        workers[i] = thread(worker, i + 1);
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        workers[i].join();
-    }
-
+    critical_section = -1;
+    cout << "\nCritical Section is Now Empty\nAll Process have been executed";
     return 0;
 }
